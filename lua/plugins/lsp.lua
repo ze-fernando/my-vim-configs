15eb39b4a12cbd644407de74992842400393bcd7
ze-fernando <zefernando.dev@gmail.com>
2025-08-11 09:40:33 -0300

    feat: remove some lsps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,50 +1,95 @@
--- Exemplo plugins/lsp.lua
 return {
-  "VonHeikemen/lsp-zero.nvim",
-  branch = "v1.x",
-  config = function()
-    local lsp = require("lsp-zero").preset({
-      name = "minimal",
-      set_lsp_keymaps = true,
-      manage_nvim_cmp = true,
-      suggest_lsp_servers = true,
-    })
+	"VonHeikemen/lsp-zero.nvim",
+	branch = "v1.x",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lua",
+		"L3MON4D3/LuaSnip",
+		"rafamadriz/friendly-snippets",
 
-    lsp.on_attach(function(client, bufnr)
-      local opts = { buffer = bufnr, remap = false }
+		{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+		{ "EmranMR/tree-sitter-blade" },
 
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-      vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format { async = true } end, opts)
+		{ "mhartington/formatter.nvim" },
+	},
+	config = function()
+		local lsp = require("lsp-zero").preset({})
 
-      if client.server_capabilities.documentFormattingProvider then
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          buffer = bufnr,
-          callback = function()
-            vim.lsp.buf.format({ bufnr = bufnr })
-          end,
-        })
-      end
-    end)
+		lsp.on_attach(function(client, bufnr)
+			local opts = { buffer = bufnr, remap = false }
 
-    lsp.ensure_installed({ "intelephense" })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+			vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format { async = true } end, opts)
 
-    lsp.setup()
-  end,
-  dependencies = {
-    "neovim/nvim-lspconfig",
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "saadparwaiz1/cmp_luasnip",
-    "hrsh7th/cmp-nvim-lua",
-    "L3MON4D3/LuaSnip",
-    "rafamadriz/friendly-snippets",
-  },
+			if client.server_capabilities.documentFormattingProvider then
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					buffer = bufnr,
+					callback = function()
+						vim.lsp.buf.format({ bufnr = bufnr })
+					end,
+				})
+			end
+		end)
+
+		lsp.ensure_installed({
+			"pyright", -- Python
+			"tsserver", -- JS/TS/TSX
+			"rust_analyzer", -- Rust
+			-- "clangd", -- C/C++
+			-- "omnisharp", -- C#
+			-- "jdtls", -- Java
+			"intelephense", -- PHP
+		})
+
+		lsp.setup()
+
+		-- Treesitter blade
+		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+		parser_config.blade = {
+			install_info = {
+				url = "https://github.com/EmranMR/tree-sitter-blade",
+				files = { "src/parser.c" },
+				branch = "main",
+			},
+			filetype = "blade",
+		}
+		vim.filetype.add({
+			pattern = {
+				[".*%.blade%.php"] = "blade",
+			},
+		})
+
+		-- formatter.nvim blade
+		require("formatter").setup({
+			logging = false,
+			filetype = {
+				blade = {
+					function()
+						return {
+							exe = "blade-formatter",
+							args = { "--stdin" },
+							stdin = true,
+						}
+					end,
+				},
+			},
+		})
+
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*.blade.php",
+			callback = function()
+				vim.cmd("Format")
+			end,
+		})
+	end,
 }
-
